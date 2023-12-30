f87d2bfb41f9fad402f9a25c8ccb1b187bb89b4c
Mahesh Shastrakar <maheshshastrakar123@gmail.com>
2023-12-30 07:17:32 -0500

    django proxy lab complete

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,23 +1,22 @@
 from django.contrib import admin
-# from .models import related models
 from .models import CarMake, CarModel
 
 
 # Register your models here.
+admin.site.register(CarMake)
+
+admin.site.register(CarModel)
+
+
 
 # CarModelInline class
+
+class CarModelInline:
+    pass
+
 
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
-@admin.register(CarMake)
-class CarMakeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')  # Display fields in the admin list view
-
-@admin.register(CarModel)
-class CarModelAdmin(admin.ModelAdmin):
-    list_display = ('car_make', 'name', 'dealer_id', 'type', 'year')  # Display fields in the admin list view
-    list_filter = ('car_make', 'type', 'year')  # Add filters on the right side
-    search_fields = ('car_make__name', 'name', 'dealer_id')  # Add search functionality
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,31 +4,95 @@ from django.utils.timezone import now
 
 # Create your models here.
 
+# <HINT> Create a Car Make model `class CarMake(models.Model)`:
+# - Name
+# - Description
+# - Any other fields you would like to include in car make model
+# - __str__ method to print a car make object
 class CarMake(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    
+    name = models.CharField(max_length=50, primary_key=True)
+    desc = models.CharField(max_length=200)
+
     def __str__(self):
         return self.name
 
 
+
+# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
+# - Name
+# - Dealer id, used to refer a dealer created in cloudant database
+# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
+# - Year (DateField)
+# - Any other fields you would like to include in car model
+# - __str__ method to print a car make object
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    dealer_id = models.CharField(max_length=50)  # Assuming dealer_id is a string
-    CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-    ]
-    type = models.CharField(max_length=20, choices=CAR_TYPES)
-    year = models.DateField()
-    
+    name = models.CharField(max_length=50, primary_key=True)
+    id = models.CharField(max_length=2)
+    type = models.CharField(max_length=10)
+    year = models.DateField(null=True)
+    carmakes = models.ManyToManyField(CarMake)
+
     def __str__(self):
-        return f"{self.car_make.name} - {self.name}"
+        return self.name
+
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
+class CarDealer:
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
+
+
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+
+class DealerReview:
+
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        # Dealership
+        self.dealership = dealership
+        # Dealer name
+        self.name = name
+        # Dealer purchase
+        self.purchase = purchase
+        # Dealer review
+        self.review = review
+        # purchase_date
+        self.purchase_date = purchase_date
+        # car_make
+        self.car_make = car_make
+        # car_model
+        self.car_model = car_model
+        # car_year
+        self.car_year = car_year
+        # sentiment
+        self.sentiment = sentiment
+        # id
+        self.id = id
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
